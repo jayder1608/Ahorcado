b06930bb9acf7c83318837eda59542adc5da2063
jayder1608 <jaydercalarodriguez@gmail.com>
2025-11-27 19:11:43 -0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+
+export default defineConfig({
+  plugins: [
+    vue({ template: { transformAssetUrls }}),
+    quasar({
+      sassVariables: fileURLToPath(
+        new URL('./src/quasar-variables.sass', import.meta.url)
+      )
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
